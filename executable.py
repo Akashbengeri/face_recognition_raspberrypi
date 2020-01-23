70ae44e93ee35403ae2ccb94f992793cbf4c168a
Pawas Suman (RBEI/ESD-CV) <Suman.Pawas@in.bosch.com>
2020-01-23 15:24:05 +0530

    Initial

---
+++ executable.py
@@ -0,0 +1,5 @@
+import os
+
+#pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+os.system("python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle" )
+input() 
